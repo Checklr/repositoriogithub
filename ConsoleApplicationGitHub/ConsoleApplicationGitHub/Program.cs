dcae9f9f2fb10369ddeb0a1b23edad996582db61
Checklr <Gringopc96@hotmail.com>
2015-07-30 13:48:38 -0300

    escribi un console.write pa proba

--- ConsoleApplicationGitHub/ConsoleApplicationGitHub/Program.cs
+++ ConsoleApplicationGitHub/ConsoleApplicationGitHub/Program.cs
@@ -10,6 +10,8 @@ namespace ConsoleApplicationGitHub
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("probando git hub");
+            Console.ReadLine();
         }
     }
 }
